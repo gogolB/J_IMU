a4eeff8fd8155d3de0c0cc0d68c3659d350137b0
unknown <sbhattacharya@GOGOL-WORK.engr.local>
2016-02-26 14:31:22 -0800

    UI/UX work
    removed the mail floating button
    added in serial output switch
    added in proper string resources

--- app/src/main/java/com/kassas_lab/j_imu/SetUp_IMU.java
+++ app/src/main/java/com/kassas_lab/j_imu/SetUp_IMU.java
@@ -17,15 +17,6 @@ public class SetUp_IMU extends AppCompatActivity {
         setContentView(R.layout.activity_set_up__imu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
