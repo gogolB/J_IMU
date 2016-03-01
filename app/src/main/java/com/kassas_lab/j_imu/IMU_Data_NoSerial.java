5e47460a537f5fc23bb2e42358d53f82cee4db5b
unknown <sbhattacharya@GOGOL-WORK.engr.local>
2016-03-01 15:00:42 -0800

    Started the view where you can see the data as it is being recorded.

---
+++ app/src/main/java/com/kassas_lab/j_imu/IMU_Data_NoSerial.java
@@ -0,0 +1,13 @@
+package com.kassas_lab.j_imu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IMU_Data_NoSerial extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imu__data__no_serial);
+    }
+}
--- app/src/main/java/com/kassas_lab/j_imu/SetUp_IMU.java
+++ app/src/main/java/com/kassas_lab/j_imu/SetUp_IMU.java
@@ -1,6 +1,11 @@
 package com.kassas_lab.j_imu;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,13 +14,83 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Switch;
+import android.widget.TextView;
 
-public class SetUp_IMU extends Activity{
+import java.util.List;
+
+public class SetUp_IMU extends Activity
+{
+
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_up__imu);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        TextView t;
+        Switch s;
+
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null)
+        {
+            t = (TextView)findViewById(R.id.AccPresent);
+            t.setText(R.string.Present);
+        }
+        else
+        {
+            t = (TextView)findViewById(R.id.AccPresent);
+            t.setText(R.string.NotPresent);
+            s = (Switch)findViewById(R.id.AccCor);
+            s.setEnabled(false);
+            s = (Switch)findViewById(R.id.AccRaw);
+            s.setEnabled(false);
+        }
+
+
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null)
+        {
+            t = (TextView)findViewById(R.id.GyroPresent);
+            t.setText(R.string.Present);
+            if(sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE_UNCALIBRATED) == null)
+            {
+                s = (Switch)findViewById(R.id.GyroRaw);
+                s.setEnabled(false);
+            }
+
+        }
+        else
+        {
+            t = (TextView)findViewById(R.id.GyroPresent);
+            t.setText(R.string.NotPresent);
+            s = (Switch)findViewById(R.id.GyroRaw);
+            s.setEnabled(false);
+            s = (Switch)findViewById(R.id.GyroCor);
+            s.setEnabled(false);
+        }
+
+
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)!= null)
+        {
+            t = (TextView)findViewById(R.id.MagPresent);
+            t.setText(R.string.Present);
+            if(sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED) == null)
+            {
+                s = (Switch)findViewById(R.id.MagRaw);
+                s.setEnabled(false);
+            }
+        }
+        else
+        {
+            t = (TextView)findViewById(R.id.MagPresent);
+            t.setText(R.string.NotPresent);
+            s = (Switch)findViewById(R.id.MagRaw);
+            s.setEnabled(false);
+            s = (Switch)findViewById(R.id.MagCor);
+            s.setEnabled(false);
+        }
+
+        setTitle(R.string.SetUpTitleString);
     }
 
     @Override
@@ -38,5 +113,35 @@ public class SetUp_IMU extends Activity{
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onInitButtonPress(View view)
+    {
+        Switch s = (Switch)findViewById(R.id.OutputOverSerial);
+        if(s.isChecked())
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            // The okay button
+            builder.setPositiveButton(R.string.Okay, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+
+            // User hit the cancel button.
+            builder.setNegativeButton(R.string.Cancel, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+
+            AlertDialog alertDialog = builder.create(); //Read Update
+            alertDialog.setTitle("Serial Output");
+            alertDialog.setMessage("You have selected to output the data over serial. It will output at a rate of 9600 BAUD.");
+
+            alertDialog.show();  //<-- See This!
+        }
     }
 }
